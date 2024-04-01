e4ca7330bd2f6ef3bbeff1be93f6d26c923c5b9c
もっちゃん <s7056i3056@gmail.com>
2024-04-01 23:55:31 +0900

    add: ブログメッセージの編集Linkを追加

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../../styles/Home.module.css";
 import { PostType } from "../../utils/Type";
 import { getAllPosts, getPostById } from "../../utils/api";
+import Link from "next/link";
 
 type Props = {
   post: PostType;
@@ -34,6 +35,9 @@ const Post = ({ post }: Props) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{post.title}</h1>
+      <button>
+        <Link className={styles.editBtn} href={`/edit/${post.id}`}>edit</Link>
+      </button>
       <p className={styles.content}>{post.content}</p>
       <p className={styles.meta}>Author: {post.author}</p>
       <p className={styles.meta}>Create At: {post.createdAt}</p>
